be40c387bdc0e18537843c294c790e5aa165add7
iftikarjahan <iftikarjahan22@gmail.com>
2024-11-08 12:00:27 +0530

    basic setup made using html project

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  /*
+  ->The strict mode only runs in the development mode. It does not run in the production env
+  ->It runs the component twice
+
+  ->Note an important thing that you can create multiple css files but the changes wont be
+  reflected individually on specific file
+  ->In order to solve this, you use the styled component.
+  */ 
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
